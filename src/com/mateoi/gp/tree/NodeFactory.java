a8d828df5d7831bba152aff8e2e90c0fe78cd29a
mateo i <mateo i>
2016-10-27 09:39:50 +0100

    Engine now runs with NoRules

--- src/com/mateoi/gp/tree/NodeFactory.java
+++ src/com/mateoi/gp/tree/NodeFactory.java
@@ -8,52 +8,73 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+import com.mateoi.gp.tree.functions.Constant;
+
 @SuppressWarnings("rawtypes")
 public class NodeFactory {
 
-    private static final NodeFactory instance = new NodeFactory();
-    private final Map<Class, List<Function<Integer, Node>>> typeConstructors = new HashMap<>();
-    private final Map<Class, List<Supplier<Node>>> terminalConstructors = new HashMap<>();
-    private final List<BiFunction<Class, Integer, Node>> genericConstructors = new ArrayList<>();
+	private static final NodeFactory instance = new NodeFactory();
+	private final Map<Class, List<Function<Integer, Node>>> typeConstructors = new HashMap<>();
+	private final Map<Class, List<Supplier<Node>>> terminalConstructors = new HashMap<>();
+	private final List<BiFunction<Class, Integer, Node>> genericConstructors = new ArrayList<>();
 
-    public Node createRandomNode(Class type, int depth) {
-        if (depth <= 0) {
-            return createTerminal(type);
-        } else {
-            return createAnyNode(type, depth);
-        }
-    }
+	public Node createRandomNode(Class type, int depth) {
+		if (depth <= 0) {
+			return createTerminal(type);
+		} else {
+			return createAnyNode(type, depth);
+		}
+	}
 
-    private Node createTerminal(Class type) {
-        final List<Supplier<Node>> list = this.terminalConstructors.get(type);
-        final int index = randInt(list.size());
-        return list.get(index).get();
-    }
+	private Node createTerminal(Class type) {
+		final List<Supplier<Node>> list = terminalConstructors.get(type);
+		final int index = randInt(list.size());
+		return list.get(index).get();
+	}
 
-    private Node createAnyNode(Class type, int depth) {
-        final List<Function<Integer, Node>> constructors = this.typeConstructors.get(type);
-        final int total = constructors.size() + this.genericConstructors.size();
-        final int index = randInt(total);
-        if (index < constructors.size()) {
-            final Function<Integer, Node> c = constructors.get(index);
-            return c.apply(depth);
-        } else {
-            final BiFunction<Class, Integer, Node> c = this.genericConstructors.get(index - constructors.size());
-            return c.apply(type, depth);
-        }
-    }
+	private Node createAnyNode(Class type, int depth) {
+		final List<Function<Integer, Node>> constructors = typeConstructors.get(type);
+		final int total = constructors.size() + genericConstructors.size();
+		final int index = randInt(total);
+		if (index < constructors.size()) {
+			final Function<Integer, Node> c = constructors.get(index);
+			return c.apply(depth);
+		} else {
+			final BiFunction<Class, Integer, Node> c = genericConstructors.get(index - constructors.size());
+			return c.apply(type, depth);
+		}
+	}
 
-    private int randInt(int max) {
-        final double rand = Math.random();
-        return (int) (rand * max);
-    }
+	private int randInt(int max) {
+		final double rand = Math.random();
+		return (int) (rand * max);
+	}
 
-    private NodeFactory() {
-        // private to prevent instantiation
-    }
+	private NodeFactory() {
+		// private to prevent instantiation
 
-    public static NodeFactory getInstance() {
-        return instance;
-    }
+		final List<Supplier<Node>> doubleTerminals = new ArrayList<>();
+		doubleTerminals.add(() -> Constant.doubleConstant(0.0));
+		doubleTerminals.add(() -> Constant.doubleConstant(1.0));
+		doubleTerminals.add(() -> Constant.doubleConstant(-1.0));
+		doubleTerminals.add(() -> Constant.doubleConstant(2.0));
+		doubleTerminals.add(() -> Constant.doubleConstant(-2.0));
+		terminalConstructors.put(Double.class, doubleTerminals);
+
+		final List<Function<Integer, Node>> doubles = new ArrayList<>();
+		putTerminalsInNodes(doubleTerminals, doubles);
+		typeConstructors.put(Double.class, doubles);
+
+	}
+
+	private static void putTerminalsInNodes(List<Supplier<Node>> terminals, List<Function<Integer, Node>> nodes) {
+		for (final Supplier<Node> terminal : terminals) {
+			nodes.add(i -> terminal.get());
+		}
+	}
+
+	public static NodeFactory getInstance() {
+		return instance;
+	}
 
 }
--- src/com/mateoi/gp/tree/functions/Constant.java
+++ src/com/mateoi/gp/tree/functions/Constant.java
@@ -5,28 +5,28 @@ import com.mateoi.gp.tree.Arity0Node;
 
 public class Constant<T> extends Arity0Node<T> {
 
-    private final T value;
+	private final T value;
 
-    public Constant(T value, Class<T> type) {
-        super(value.toString(), type);
-        this.value = value;
-    }
+	public Constant(T value, Class<T> type) {
+		super(value.toString(), type);
+		this.value = value;
+	}
 
-    @Override
-    public T evaluate() throws TypeMismatch {
-        return this.value;
-    }
+	@Override
+	public T evaluate() throws TypeMismatch {
+		return this.value;
+	}
 
-    public static Constant<Double> doubleConstant(double value) {
-        return new Constant<>(value, Double.class);
-    }
+	public static Constant<Double> doubleConstant(double value) {
+		return new Constant<>(value, Double.class);
+	}
 
-    public static Constant<Integer> doubleConstant(int value) {
-        return new Constant<>(value, Integer.class);
-    }
+	public static Constant<Integer> intConstant(int value) {
+		return new Constant<>(value, Integer.class);
+	}
 
-    public static Constant<Boolean> doubleConstant(boolean value) {
-        return new Constant<>(value, Boolean.class);
-    }
+	public static Constant<Boolean> boolConstant(boolean value) {
+		return new Constant<>(value, Boolean.class);
+	}
 
 }
